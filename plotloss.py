fe0c5217215fc05f02723725ad65e59ae46ecd84
a-capra <andrea.capra85@gmail.com>
2019-09-06 19:22:30 -0700

    change to the useless large number of epochs

--- plotloss.py
+++ plotloss.py
@@ -6,11 +6,13 @@ def moving_average(a, n=3) :
     ret[n:] = ret[n:] - ret[:-n]
     return ret[n - 1:] / n
 
-sample_size=50
-x = np.arange(sample_size-1,50000)
+sample_size=100
+x = np.arange(sample_size-1,100000)
 d = np.loadtxt('dlosses.txt', dtype=float)
 g = np.loadtxt('glosses.txt', dtype=float)
 plt.plot(x,moving_average(d,sample_size),'r.',label='discriminator')
 plt.plot(x,moving_average(g,sample_size),'b.',label='generator')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
 plt.legend(loc='best')
 plt.show()
